5eccfc5102bba26483aeb6a6afe7089aac2c4c9e
Angular CLI <angular-cli@angular.io>
2018-07-11 15:41:13 +0300

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nla-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'nla';
+}
